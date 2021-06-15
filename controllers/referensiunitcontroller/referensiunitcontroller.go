b4a7dca808bc227597058f97cfe7d34c225b5b62
antoniuscahyo <antoniuscahyo18@gmail.com>
2021-06-16 00:42:06 +0700

    Update Modul CRUD Unit

--- controllers/referensiunitcontroller/referensiunitcontroller.go
+++ controllers/referensiunitcontroller/referensiunitcontroller.go
@@ -4,7 +4,10 @@ import (
 	"html/template"
 	"net/http"
 	"fmt"
+	"SMKPUPY/models"	
+	"SMKPUPY/entities"
 	"github.com/kataras/go-sessions"
+	"strconv"
 )
 
 func Index(response http.ResponseWriter, request *http.Request) {
@@ -14,7 +17,11 @@ func Index(response http.ResponseWriter, request *http.Request) {
 		http.Redirect(response, request, "/login", 301)
 	}
 
+	var unitModel models.UnitModel
+	unit, _ := unitModel.FindAll()
+
 	data := map[string]interface{} {
+		"records": unit,
 		"username":      session.GetString("username"),
 		"message":       "Welcome to the Go !",
 		"nama_pengguna": session.GetString("nama"),
@@ -40,16 +47,98 @@ func Index(response http.ResponseWriter, request *http.Request) {
 }
 
 func Add(response http.ResponseWriter, request *http.Request) {
+	session := sessions.Start(response, request)
+	if len(session.GetString("username")) == 0 {
+		http.Redirect(response, request, "/login", 301)
+	}
+
+	data := map[string]interface{} {
+		"username":      session.GetString("username"),
+		"message":       "Welcome to the Go !",
+		"nama_pengguna": session.GetString("nama"),
+		"Idrole":        session.GetString("Idrole"),
+		"NamaAplikasi":  "SMKP UPY",
+	}
+
+	var t, err = template.ParseFiles(
+		"views/referensi_unit/add.html",
+		"views/template/_header.html",
+		"views/template/_footer.html",
+		"views/template/_sidebar.html",
+	)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		http.Error(response, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	t.Execute(response, data)
+	return
 }
 
 func ProcessAdd(response http.ResponseWriter, request *http.Request) {
+	request.ParseForm()
+	var unit entities.Unit
+	unit.UnitNama = request.Form.Get("UnitNama")
+	unit.UnitLevel = request.Form.Get("UnitLevel")
+	var unitModel models.UnitModel
+	unitModel.Create(&unit)
+	http.Redirect(response, request, "/referensi_unit", http.StatusSeeOther)
 }
 
 func Update(response http.ResponseWriter, request *http.Request) {
+	request.ParseForm()
+	var unit entities.Unit
+	unit.IdUnit, _ = strconv.ParseInt(request.Form.Get("IdUnit"), 10, 64)
+	unit.UnitNama = request.Form.Get("UnitNama")
+	unit.UnitLevel = request.Form.Get("UnitLevel")
+	var unitModel models.UnitModel
+	unitModel.Update(unit)
+	http.Redirect(response, request, "/referensi_unit", http.StatusSeeOther)
 }
 
 func Delete(response http.ResponseWriter, request *http.Request) {
+	query := request.URL.Query()
+	id, _ := strconv.ParseInt(query.Get("id"), 10, 64)
+	var unitModel models.UnitModel
+	unitModel.Delete(id)
+	http.Redirect(response, request, "/referensi_unit", http.StatusSeeOther)
 }
 
 func Edit(response http.ResponseWriter, request *http.Request) {
+	session := sessions.Start(response, request)
+	if len(session.GetString("username")) == 0 {
+		http.Redirect(response, request, "/login", 301)
+	}
+
+	query := request.URL.Query()
+	id, _ := strconv.ParseInt(query.Get("id"), 10, 64)
+	var unitModel models.UnitModel
+	unit, _ := unitModel.Find(id)
+	
+	data := map[string]interface{} {
+		"records": unit,
+		"username":      session.GetString("username"),
+		"message":       "Welcome to the Go !",
+		"nama_pengguna": session.GetString("nama"),
+		"Idrole":        session.GetString("Idrole"),
+		"NamaAplikasi":  "SMKP UPY",
+	}
+
+	var t, err = template.ParseFiles(
+		"views/referensi_unit/edit.html",
+		"views/template/_header.html",
+		"views/template/_footer.html",
+		"views/template/_sidebar.html",
+	)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		http.Error(response, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	t.Execute(response, data)
+	return
 }
---
+++ models/unitmodel.go
@@ -0,0 +1,93 @@
+package models
+
+import (
+	"SMKPUPY/config"
+	"SMKPUPY/entities"
+)
+
+type UnitModel struct {
+
+}
+
+func (*UnitModel) FindAll() ([]entities.Unit, error) {
+	db, err := config.GetDB()
+	if	err != nil {
+		return nil, err
+	} else {
+		rows, err2 := db.Query(`SELECT unit_id, unit_nama, unit_level FROM unit`)
+		if err2 != nil {
+			return nil,err2
+		} else {
+			var units []entities.Unit
+			for rows.Next() {
+				var unit entities.Unit
+				rows.Scan(&unit.IdUnit, &unit.UnitNama, &unit.UnitLevel)
+				units = append(units, unit)
+			}
+			return units, nil
+		}
+	}
+}
+
+func (*UnitModel) Find(id int64) (entities.Unit, error) {
+	db, err := config.GetDB()
+	if	err != nil {
+		return entities.Unit{}, err
+	} else {
+		rows, err2 := db.Query("SELECT unit_id, unit_nama, unit_level FROM unit WHERE unit_id = ?", id)
+		if err2 != nil {
+			return entities.Unit{}, err2
+		} else {
+			var unit entities.Unit
+			for rows.Next() {
+				rows.Scan(&unit.IdUnit, &unit.UnitNama, &unit.UnitLevel)
+			}
+			return unit, nil
+		}
+	}
+}
+
+func (*UnitModel) Create(unit *entities.Unit) bool {
+	db, err := config.GetDB()
+	if	err != nil {
+		return false
+	} else {
+		result, err2 := db.Exec("INSERT INTO unit(unit_nama, unit_level) VALUES (?,?)", unit.UnitNama, unit.UnitLevel)
+		if err2 != nil {
+			return false
+		} else {
+			rowsAffected, _ := result.RowsAffected()
+			return rowsAffected > 0
+		}
+	}
+}
+
+func (*UnitModel) Update(unit entities.Unit) bool {
+	db, err := config.GetDB()
+	if	err != nil {
+		return false
+	} else {
+		result, err2 := db.Exec("UPDATE unit SET unit_nama = ?, unit_level = ? WHERE unit_id = ?", unit.UnitNama, unit.UnitLevel, unit.IdUnit)
+		if err2 != nil {
+			return false
+		} else {
+			rowsAffected, _ := result.RowsAffected()
+			return rowsAffected > 0
+		}
+	}
+}
+
+func (*UnitModel) Delete(id int64) bool {
+	db, err := config.GetDB()
+	if	err != nil {
+		return false
+	} else {
+		result, err2 := db.Exec("DELETE FROM unit WHERE unit_id = ?", id)
+		if err2 != nil {
+			return false
+		} else {
+			rowsAffected, _ := result.RowsAffected()
+			return rowsAffected > 0
+		}
+	}
+}
