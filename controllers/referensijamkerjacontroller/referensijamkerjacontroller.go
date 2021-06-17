ea0d03d47c23141396090a5742c956158e4aab98
antoniuscahyo <antoniuscahyo18@gmail.com>
2021-06-17 09:18:45 +0700

    update menu referensi jam kerja

--- controllers/referensijamkerjacontroller/referensijamkerjacontroller.go
+++ controllers/referensijamkerjacontroller/referensijamkerjacontroller.go
@@ -16,8 +16,8 @@ func Index(response http.ResponseWriter, request *http.Request) {
 		http.Redirect(response, request, "/login", 301)
 	}
 
-	var unitJamKerja models.UnitJamKerja
-	jamkerja, _ := unitJamKerja.FindAll()
+	var jamkerjaModel models.JamKerjaModel
+	jamkerja, _ := jamkerjaModel.FindAll()
 
 	data := map[string]interface{} {
 		"records": jamkerja,
@@ -47,8 +47,6 @@ func Index(response http.ResponseWriter, request *http.Request) {
 
 
 func Add(response http.ResponseWriter, request *http.Request) {
-	/*
-	fmt.Println("Run add")
 	session := sessions.Start(response, request)
 	if len(session.GetString("username")) == 0 {
 		http.Redirect(response, request, "/login", 301)
@@ -63,7 +61,7 @@ func Add(response http.ResponseWriter, request *http.Request) {
 	}
 
 	var t, err = template.ParseFiles(
-		"views/product/add.html",
+		"views/referensi_jam_kerja/add.html",
 		"views/template/_header.html",
 		"views/template/_footer.html",
 		"views/template/_sidebar.html",
@@ -77,55 +75,65 @@ func Add(response http.ResponseWriter, request *http.Request) {
 
 	t.Execute(response, data)
 	return
-
-	// tmp, _ := template.ParseFiles("views/product/add.html")
-	// tmp.Execute(response, nil)
-	*/
 }
 
 func ProcessAdd(response http.ResponseWriter, request *http.Request) {
-	/*
 	request.ParseForm()
-	var product entities.Product
-	product.Name = request.Form.Get("name")
-	product.Price, _ = strconv.ParseFloat(request.Form.Get("price"), 64)
-	product.Quantity, _ = strconv.ParseInt(request.Form.Get("quantity"), 10, 64)
-	product.Description = request.Form.Get("description")
-	var productModel models.ProductModel
-	productModel.Create(&product)
-	http.Redirect(response, request, "/product", http.StatusSeeOther)
-	*/
+	var jamkerja entities.JamKerja
+	jamkerja.NamaJamKerja = request.Form.Get("NamaJamKerja")
+	jamkerja.JamMasukSenin = request.Form.Get("JamMasukSenin")
+	jamkerja.JamPulangSenin = request.Form.Get("JamPulangSenin")
+	jamkerja.JamMasukSelasa = request.Form.Get("JamMasukSelasa")
+	jamkerja.JamPulangSelasa = request.Form.Get("JamPulangSelasa")
+	jamkerja.JamMasukRabu = request.Form.Get("JamMasukRabu")
+	jamkerja.JamPulangRabu = request.Form.Get("JamPulangRabu")
+	jamkerja.JamMasukKamis = request.Form.Get("JamMasukKamis")
+	jamkerja.JamPulangKamis = request.Form.Get("JamPulangKamis")
+	jamkerja.JamMasukJumat = request.Form.Get("JamMasukJumat")
+	jamkerja.JamPulangJumat = request.Form.Get("JamPulangJumat")
+	jamkerja.JamMasukSabtu = request.Form.Get("JamMasukSabtu")
+	jamkerja.JamPulangSabtu = request.Form.Get("JamPulangSabtu")
+	jamkerja.JamMasukMinggu = request.Form.Get("JamMasukMinggu")
+	jamkerja.JamPulangMinggu = request.Form.Get("JamPulangMinggu")
+	var jamkerjaModel models.JamKerjaModel
+	jamkerjaModel.Create(&jamkerja)
+	http.Redirect(response, request, "/referensi_jam_kerja", http.StatusSeeOther)
 }
 
 func Update(response http.ResponseWriter, request *http.Request) {
-	/*
 	request.ParseForm()
-	var product entities.Product
-	product.Id, _ = strconv.ParseInt(request.Form.Get("id"), 10, 64)
-	product.Name = request.Form.Get("name")
-	product.Price, _ = strconv.ParseFloat(request.Form.Get("price"), 64)
-	product.Quantity, _ = strconv.ParseInt(request.Form.Get("quantity"), 10, 64)
-	product.Description = request.Form.Get("description")
-	var productModel models.ProductModel
-	productModel.Update(product)
-	http.Redirect(response, request, "/product", http.StatusSeeOther)
-	*/
+	var jamkerja entities.JamKerja
+	jamkerja.IdJamKerja, _ = strconv.ParseInt(request.Form.Get("IdJamKerja"), 10, 64)
+	jamkerja.NamaJamKerja = request.Form.Get("NamaJamKerja")
+	jamkerja.JamMasukSenin = request.Form.Get("JamMasukSenin")
+	jamkerja.JamPulangSenin = request.Form.Get("JamPulangSenin")
+	jamkerja.JamMasukSelasa = request.Form.Get("JamMasukSelasa")
+	jamkerja.JamPulangSelasa = request.Form.Get("JamPulangSelasa")
+	jamkerja.JamMasukRabu = request.Form.Get("JamMasukRabu")
+	jamkerja.JamPulangRabu = request.Form.Get("JamPulangRabu")
+	jamkerja.JamMasukKamis = request.Form.Get("JamMasukKamis")
+	jamkerja.JamPulangKamis = request.Form.Get("JamPulangKamis")
+	jamkerja.JamMasukJumat = request.Form.Get("JamMasukJumat")
+	jamkerja.JamPulangJumat = request.Form.Get("JamPulangJumat")
+	jamkerja.JamMasukSabtu = request.Form.Get("JamMasukSabtu")
+	jamkerja.JamPulangSabtu = request.Form.Get("JamPulangSabtu")
+	jamkerja.JamMasukMinggu = request.Form.Get("JamMasukMinggu")
+	jamkerja.JamPulangMinggu = request.Form.Get("JamPulangMinggu")
+	var jamkerjaModel models.JamKerjaModel
+	jamkerjaModel.Update(jamkerja)
+	http.Redirect(response, request, "/referensi_jam_kerja", http.StatusSeeOther)
 }
 
 
 func Delete(response http.ResponseWriter, request *http.Request) {
-	/*
 	query := request.URL.Query()
 	id, _ := strconv.ParseInt(query.Get("id"), 10, 64)
-	var productModel models.ProductModel
-	productModel.Delete(id)
-	http.Redirect(response, request, "/product", http.StatusSeeOther)
-	*/
+	var jamkerjaModel models.JamKerjaModel
+	jamkerjaModel.Delete(id)
+	http.Redirect(response, request, "/referensi_jam_kerja", http.StatusSeeOther)
 }
 
 func Edit(response http.ResponseWriter, request *http.Request) {
-	/*
-	// fmt.Println("Edittt woy")
 	session := sessions.Start(response, request)
 	if len(session.GetString("username")) == 0 {
 		http.Redirect(response, request, "/login", 301)
@@ -133,14 +141,11 @@ func Edit(response http.ResponseWriter, request *http.Request) {
 
 	query := request.URL.Query()
 	id, _ := strconv.ParseInt(query.Get("id"), 10, 64)
-	var productModel models.ProductModel
-	product, _ := productModel.Find(id)
-	
-	// data := map[string]interface{} {
-	// 	"product": product,
-	// }
+	var jamkerjaModel models.JamKerjaModel
+	jamkerja, _ := jamkerjaModel.Find(id)
+
 	data := map[string]interface{} {
-		"product": product,
+		"records": jamkerja,
 		"username":      session.GetString("username"),
 		"message":       "Welcome to the Go !",
 		"nama_pengguna": session.GetString("nama"),
@@ -148,11 +153,8 @@ func Edit(response http.ResponseWriter, request *http.Request) {
 		"NamaAplikasi":  "SMKP UPY",
 	}
 
-	// tmp, _ := template.ParseFiles("views/product/edit.html")
-	// tmp.Execute(response, data)
-
 	var t, err = template.ParseFiles(
-		"views/product/edit.html",
+		"views/referensi_jam_kerja/edit.html",
 		"views/template/_header.html",
 		"views/template/_footer.html",
 		"views/template/_sidebar.html",
@@ -166,5 +168,4 @@ func Edit(response http.ResponseWriter, request *http.Request) {
 
 	t.Execute(response, data)
 	return
-	*/
 }
