5872b3229999590c8386244fba13bbee979393e3
antoniuscahyo18 <antoniuscahyo18@gmail.com>
2021-07-20 13:17:17 +0700

    setting up db connect with godotenv

--- config/config.go
+++ config/config.go
@@ -2,19 +2,21 @@ package config
 
 import (
 	"database/sql"
-	// "SMKPUPY/components"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
+// var DB_DRIVER = goDotEnvVariable("DB_DRIVER")
+// fmt.Println(DB_DRIVER)
+
 func GetDB() (db *sql.DB, err error) {
-	//DB_DRIVER := goDotEnvVariable("DB_DRIVER")
-	dbDriver := "mysql"
-	dbName := "db_monitoring_kehadiran_pegawai"
-	dbHost := "localhost"
-	dbPort := "3306"
-	dbUser := "root"
-	dbPass := ""
+
+	dbDriver := goDotEnvVariable("DB_DRIVER")
+	dbName := goDotEnvVariable("DB_NAME")
+	dbHost := goDotEnvVariable("DB_HOST")
+	dbPort := goDotEnvVariable("DB_PORT")
+	dbUser := goDotEnvVariable("DB_USER")
+	dbPass := goDotEnvVariable("DB_PASS")
 	db, err = sql.Open(dbDriver, dbUser+":"+dbPass+"@tcp("+dbHost+":"+dbPort+")/"+dbName)
 	return
 }
