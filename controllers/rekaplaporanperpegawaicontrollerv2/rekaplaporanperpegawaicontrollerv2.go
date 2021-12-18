50454d4e02dec6583b40bc42e119c33d24322e82
antoniuscahyo18 <antoniuscahyo18@gmail.com>
2021-12-18 22:10:16 +0700

    update rekap laporan perpegawai v2

--- controllers/rekaplaporanperpegawaicontrollerv2/rekaplaporanperpegawaicontrollerv2.go
+++ controllers/rekaplaporanperpegawaicontrollerv2/rekaplaporanperpegawaicontrollerv2.go
@@ -1,11 +1,13 @@
 package rekaplaporanperpegawaicontrollerv2
 
 import (
+	"SMKPUPY/config"
 	"SMKPUPY/models"
 	"fmt"
 	"html/template"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/kataras/go-sessions/v3"
 )
@@ -78,6 +80,56 @@ func LoadData(response http.ResponseWriter, request *http.Request) {
 
 	var t, err = template.ParseFiles(
 		"views/rekap_laporan_bulanan_perpegawaiv2/tabel.html",
+	)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		http.Error(response, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	t.Execute(response, data)
+	return
+}
+
+func Print(response http.ResponseWriter, request *http.Request) {
+	session := sessions.Start(response, request)
+	if len(session.GetString("username")) == 0 {
+		http.Redirect(response, request, "/login", 301)
+	}
+
+	IdUnit, _ := strconv.ParseInt(request.FormValue("IdUnit"), 10, 64)
+	IdPegawai, _ := strconv.ParseInt(request.FormValue("IdPegawai"), 10, 64)
+	Tahun := request.FormValue("Tahun")
+	Bulan := request.FormValue("Bulan")
+
+	IdRole, _ := strconv.ParseInt(session.GetString("Idrole"), 10, 64)
+
+	var laporanModel models.LaporanModelv2
+	detailharian, _ := laporanModel.DetailHarianv2(Tahun, Bulan, IdUnit, IdPegawai, IdRole)
+
+	var unitModel models.UnitModel
+	unit, _ := unitModel.Find(IdUnit)
+
+	var pegawaiModel models.PegawaiModel
+	pegawai, _ := pegawaiModel.Find(IdPegawai)
+
+	currentTime := time.Now()
+	timenow := currentTime.Format("02") + " " + config.GetBulan(currentTime.Format("01")) + " " + currentTime.Format("2006")
+
+	data := map[string]interface{}{
+		"data":         detailharian,
+		"nama_unit":    unit.UnitNama,
+		"nama_pegawai": pegawai.NamaPegawai,
+		"pin_finger":   pegawai.PinFinger,
+		"bulan":        config.GetBulan(Bulan),
+		"tahun":        Tahun,
+		"timenow":      timenow,
+		"NamaAplikasi": session.GetString("nama_aplikasi"),
+	}
+
+	var t, err = template.ParseFiles(
+		"views/rekap_laporan_bulanan_perpegawaiv2/printv2.html",
 	)
 
 	if err != nil {
